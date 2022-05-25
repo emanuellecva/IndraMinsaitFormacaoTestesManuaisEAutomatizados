da9e12ef31c113bbc0e3c85e709c43a525919ccb
Emanuelle <emanuelle@MacBook-Pro-de-Emanuelle.local>
2022-05-24 21:58:09 -0300

    alteracao uso do faker

--- cypress/integration/formularioIndraMinsait.spec.js
+++ cypress/integration/formularioIndraMinsait.spec.js
@@ -1,10 +1,10 @@
 
 /// <reference types ="cypress" />
 //usei biblioteca do node 'npm faker-br' para ficar gerando novos usuários aleatórios (sempre que precisar ver documentacao dela e usar)
-//https://www.npmjs.com/package/faker-br
+
 
 var faker = require('faker-br');
-//para usar a biblioteca preciso colocar essa linha superior(tá na documentacao dela)
+
 
 describe('Formulário', () => {
     it('Validar preenchimento do formulário', () => {
@@ -12,17 +12,23 @@ describe('Formulário', () => {
         let nomeFaker = faker.name.firstName()
         let sobrenomeFaker = faker.name.lastName()
         let emailFaker = faker.internet.email(nomeFaker)
+        let street = faker.address.streetName()
+        let gender = faker.name.gender()
+        let job = faker.name.jobTitle()
+        let company = faker.company.companyName()
         
+
+
 
         cy.visit('https://automacaocombatista.herokuapp.com/users/new')
         cy.get('#user_name').type(nomeFaker)
         cy.get('#user_lastname').type(sobrenomeFaker)
         cy.get('#user_email').type(emailFaker)
-        cy.get('#user_address').type('Rua José Clementino,33')
-        cy.get('#user_university').type('UFPB')
-        cy.get('#user_profile').type('Estudante')
-        cy.get('#user_gender').type('feminino')
-        cy.get('#user_age').type('30')
+        cy.get('#user_address').type(street)
+        cy.get('#user_university').type(company)
+        cy.get('#user_profile').type(job)
+        cy.get('#user_gender').type(gender)
+        cy.get('#user_age').type('35')
         cy.get('.actions').click()
         cy.get('#notice').should('be.visible')
 
